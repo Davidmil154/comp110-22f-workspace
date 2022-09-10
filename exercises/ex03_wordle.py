c16cfede0ced19d9a1a534c24474cdf9dddd4245
David Miller <davmil@ad.unc.edu>
2022-09-09 21:43:24 -0400

    modified:   exercises/ex03_wordle.py

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,6 +2,7 @@
 
 __author__ = "730622383"
 
+
 def contains_char(test_word: str, s_char: str) -> bool:
     """Checks that a character is in the test word."""
     assert len(s_char) == 1
@@ -10,8 +11,8 @@ def contains_char(test_word: str, s_char: str) -> bool:
         if s_char[0] == test_word[i]:
             return(True)
         i += 1
-        if i == len(test_word):
-            return(False)
+    return(False)
+
 
 def emojified(player_guess: str, secret_word: str) -> str:
     """Takes a player guess, and returns the emjoified version, with green white and yellow boxes. Uses contains_char to check for white and yellow."""
@@ -24,18 +25,20 @@ def emojified(player_guess: str, secret_word: str) -> str:
         if player_guess[j] == secret_word[j]:
             return_word += GREEN_BOX
         elif contains_char(secret_word, player_guess[j]) == True:
-             return_word += YELLOW_BOX
+            return_word += YELLOW_BOX
         else:
-             return_word += WHITE_BOX
+            return_word += WHITE_BOX
         j += 1
     return(return_word)
 
+
 def input_guess(secret_len: int) -> str:
     """Take input for a guess and returns it."""
-    player_guess: str = input(f"Enter a {secret_len} character word:")
+    player_guess: str = input(f"Enter a {secret_len} character word: ")
     while len(player_guess) != secret_len:
-        player_guess = input(f"That wasn't {secret_len} chars! Try again:")
+        player_guess = input(f"That wasn't {secret_len} chars! Try again: ")
     return(player_guess)
+
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
@@ -50,11 +53,10 @@ def main() -> None:
         print(emojified(player_guess, secret_word))
         if player_guess == secret_word:
             print(f"You won in {turn}/6 turns!")
-            return()
-        elif turn == 6:
-            print(f"X/6 - Sorry, try again tomorrow!")
-            return()
+            return
         turn += 1
+    print("X/6 - Sorry, try again tomorrow!")
+
 
 if __name__ == "__main__":
     main()
