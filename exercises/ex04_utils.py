bf09fb3e4ee34e63baccdd2b437a11fa5b6d9bb9
David Miller <davmil@ad.unc.edu>
2022-09-27 17:35:51 -0400

    modified:   exercises/ex04_utils.py 	new file:   exercises/ex05/utils.py 	new file:   exercises/ex05/utils_test.py

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,18 +1,25 @@
-"""EX04 - List Utilities"""
+"""EX04 - List Utilities."""
+
 
 __author__ = "730622383"
 
-def all(test_list: list[int], test_int:int) -> bool:
+
+def all(test_list: list[int], test_int: int) -> bool:
+    """Determines weather or not every element of the list is the same as the provided int."""
     i: int = 0
+    if len(test_list) == 0:
+        return (False)
     while i < len(test_list):
         if test_list[i] != test_int:
-            return(False)
+            return (False)
         i += 1
-    return(True)
+    return (True)
+
 
 def max(test_list: list[int]) -> int:
+    """Determines the maximum number in a provided list of ints."""
     i: int = 0
-    holder: int = 0
+    holder: int = test_list[0]
     if len(test_list) == 0:
         raise ValueError("max() arg is an empty list")
     while i < len(test_list):
@@ -21,15 +28,16 @@ def max(test_list: list[int]) -> int:
             i += 1
         else:
             i += 1
-    return(holder)
+    return (holder)
+
 
 def is_equal(test_list_1: list[int], test_list_2: list[int]) -> bool:
+    """Determines weather or not two lists are exactly equal."""
     if len(test_list_1) != len(test_list_2):
-        return(False)
+        return (False)
     i: int = 0
     while i < len(test_list_1):
         if test_list_1[i] != test_list_2[i]:
-            return(False)
+            return (False)
         i += 1
-    return(True)
-
+    return (True)
---
+++ exercises/ex05/utils.py
@@ -0,0 +1,33 @@
+"""EX05 - List Utility Functions."""
+
+
+__author__ = "730622383"
+
+
+def only_evens(given: list[int]) -> list[int]:
+    """Returns a list containing only the even numbers of the provided list."""
+    returner: list[int] = []
+    for num in given:
+        if num % 2 == 0:
+            returner.append(num)
+    return returner
+
+def concat(first: list[int], second: list[int]) -> list[int]:
+    """Returns a new list, containing the first list followed by the second list."""
+    for num in second:
+        first.append(num)
+    return first
+
+def sub(givenL: list[int], ind1: int, ind2: int) -> list[int]:
+    """Takes a list and two ints(for indexes), and returns a new list of all the values between the ints"""
+    returner: list[int] = []
+    if ind1 < 0:
+        i = 0
+    else:
+        i = ind1
+    if ind2 > len(givenL):
+        ind2 = len(givenL)
+    while i < ind2:
+        returner.append(givenL[i])
+        i = i + 1
+    return returner
